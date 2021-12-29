abe540c326fd31fd79880d354d247ef0e01c36d1
911 <chenjingfu.design@bytedance.com>
2021-12-29 14:02:05 +0800

    drawVector Update

--- Assets/Script/default/drawVector.cs
+++ Assets/Script/default/drawVector.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class drawVector : MonoBehaviour
 {
+    public Vector3 Vector = new Vector3(0,0,0);
+
+    public bool isNormalize = false;
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
@@ -11,7 +15,10 @@ public class drawVector : MonoBehaviour
         Gizmos.DrawWireSphere(new Vector3(0,0,0), 1f);
 
         Gizmos.color = Color.red;
+
+        //Vector3 tempVector = isNormalize? Vector.normalized : Vector;
+
         //draw Vector
-        Gizmos.DrawLine(new Vector3(0,0,0), new Vector3(1, 1, 1).normalized);
+        Gizmos.DrawLine(new Vector3(0,0,0), isNormalize ? Vector.normalized : Vector);
     }
 }
