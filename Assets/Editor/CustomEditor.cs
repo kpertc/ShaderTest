ce5a696cb8c7630ae37f00b03fbf4e69b658851c
ChenJingfu <chenjingfu.design@bytedance.com>
2022-01-17 15:56:26 +0800

    UI update
    UI Tab etc

--- Assets/Editor/CustomEditor.cs
+++ Assets/Editor/CustomEditor.cs
@@ -9,75 +9,99 @@ public class CustomEditor : EditorWindow
 
     //Variables
     private float someValue = 0;
-    public enum things 
-    {
-        cat,dog,fish
-    }
-    
+    public enum things { cat,dog,fish }
     things mythings;
     string Car_ID = "12345";
-
     Object _transform;
-
     bool foldoutState;
-    
+    private int toolbarInt;
+
     private void OnGUI()
     {
-        
-        //Label
-        GUILayout.Label("This is a Label");
-        
-        using (new GUILayout.VerticalScope(EditorStyles.helpBox)) //safe fucntion
-        {
-            //Button
-            if (GUILayout.Button("This is a button"))
-                Debug.Log("111");
-            
-            //Slider
-            GUILayout.BeginHorizontal(); //unSafe
-            
-            GUILayout.Label("Slider: ");
-            someValue = GUILayout.HorizontalSlider(someValue, -1, 1);
-            
-            GUILayout.EndHorizontal();
-            
-            //Space
-            GUILayout.Space(20);
-            
-            //EnumPop
-            mythings = (things)EditorGUILayout.EnumPopup("Choose a animal: ", mythings);
-            
-            //TextField
-            Car_ID = GUILayout.TextField(Car_ID, 25);
-            GUILayout.Label("Result: " + Car_ID);
-            
-            //ObjectField
-            _transform = EditorGUILayout.ObjectField("Text Example: ", null, typeof(Transform), true);
-            
-        }
-        
-        //Foldout
-        foldoutState = EditorGUILayout.Foldout(foldoutState, "Foldout"); // 折叠菜单状态
-        if (foldoutState)
-        {
-            EditorGUI.indentLevel++; // indentlevel only work for Editor Elements
-            
-            EditorGUILayout.LabelField("Content1");
-            EditorGUILayout.LabelField("Content1");
-            EditorGUILayout.LabelField("Content1");
-            
-            EditorGUI.indentLevel--;
-        }
-            
-        //Progress Bar
-        
-        
-        //HelpBox
-        EditorGUILayout.HelpBox("HelpBox Type:None", MessageType.None);
-        EditorGUILayout.HelpBox("HelpBox Type:Info", MessageType.Info);
-        EditorGUILayout.HelpBox("HelpBox Type:Error", MessageType.Error);
-        EditorGUILayout.HelpBox("HelpBox Type:Warning", MessageType.Warning);
+        //Tabs
+        toolbarInt = GUI.Toolbar(new Rect(3, 3, position.width - 6, 25), toolbarInt, new string[] {"基本元素", " 1", " 2"});
 
+        switch (toolbarInt)
+        {
+            case 0:
+                
+                EditorGUILayout.Space(30);
+                
+                GUILayout.Label("This is a Label");
+        
+                using (new GUILayout.VerticalScope(EditorStyles.helpBox)) //safe fucntion
+                {
+                    //Button
+                    if (GUILayout.Button("This is a button"))
+                        Debug.Log("Button Pressed");
+            
+                    //Slider
+                    GUILayout.BeginHorizontal(); //unSafe
+            
+                    GUILayout.Label("Slider: ");
+                    someValue = GUILayout.HorizontalSlider(someValue, -1, 1);
+            
+                    GUILayout.EndHorizontal();
+            
+                    //Space
+                    GUILayout.Space(20);
+            
+                    //EnumPop
+                    mythings = (things)EditorGUILayout.EnumPopup("Choose a animal: ", mythings);
+            
+                    //TextField
+                    Car_ID = GUILayout.TextField(Car_ID, 25);
+                    GUILayout.Label("Result: " + Car_ID);
+            
+                    //ObjectField
+                    _transform = EditorGUILayout.ObjectField("Text Example: ", null, typeof(Transform), true);
+            
+                }
+                
+                //PrefixLabel
+                EditorGUILayout.LabelField("PrefixLabel");
+                int ammo = 10;
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel("Ammo");  //Label to show to the left of the control.
+                ammo = EditorGUILayout.IntField(ammo);
+                EditorGUILayout.EndHorizontal();
+                
+                //Foldout
+                foldoutState = EditorGUILayout.Foldout(foldoutState, "Foldout"); // 折叠菜单状态
+                if (foldoutState)
+                {
+                    EditorGUI.indentLevel++; // indentlevel only work for Editor Elements
+            
+                    EditorGUILayout.LabelField("LabelField1");
+                    EditorGUILayout.LabelField("LabelField2");
+                    EditorGUILayout.LabelField("LabelField3");
+            
+                    EditorGUI.indentLevel--;
+                }
+                    
+                break;
+            
+            case 1:
+                
+                EditorGUILayout.Space(30);
+                
+                    
+                break;
+            
+            case 2:
+                
+                EditorGUILayout.Space(30);
+                
+                //HelpBox
+                EditorGUILayout.HelpBox("HelpBox Type:None", MessageType.None);
+                EditorGUILayout.HelpBox("HelpBox Type:Info", MessageType.Info);
+                EditorGUILayout.HelpBox("HelpBox Type:Error", MessageType.Error);
+                EditorGUILayout.HelpBox("HelpBox Type:Warning", MessageType.Warning);
+        
+                EditorGUILayout.Space(20);
+                    
+                break;
+        }
     }
     
     /*
