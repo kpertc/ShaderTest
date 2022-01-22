8141203b6c2ab5d2587533f21b6e45f3e25d22b6
kpertc <dr_cjf@126.com>
2022-01-22 17:10:13 +0800

    Color & Current Event Update

---
+++ Assets/Script/default/printCurrentEvent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class printCurrentEvent : MonoBehaviour
+{
+    void OnGUI()
+    {
+        if (Event.current.type != EventType.Repaint && Event.current.type != EventType.Layout)
+        {
+            Debug.Log("Current event detected: " + Event.current.type);
+        }
+    }
+}
