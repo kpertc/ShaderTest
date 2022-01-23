d047c7d932d5966e22a8898490dadabc6d192147
kpertc <dr_cjf@126.com>
2022-01-23 13:36:59 +0800

    CopyFileToClipBoard

---
+++ Assets/Editor/independent Functions/copyFileToClipBoard.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class copyFileToClipBoard : EditorWindow
+{
+    [MenuItem("Custom/Independent Functions/Copy File to Clipboard")]
+    public static void _copyFileToClipBoard() {
+        string[] currentSelection = Selection.assetGUIDs;
+
+        foreach (string select in currentSelection)
+        {
+            string fileSystemPath = Application.dataPath.Replace("Assets","") + AssetDatabase.GUIDToAssetPath(select);
+
+            GUIUtility.systemCopyBuffer = fileSystemPath;
+        }
+    }
+}
